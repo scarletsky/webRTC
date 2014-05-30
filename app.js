893abb602547ce8a0116f01b32d4c4fa60f7c3d0
Liucw <liucw.mail@gmail.com>
2014-05-30 17:03:38 +0800

    Add

--- app.js
+++ app.js
@@ -1,59 +1,55 @@
-var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var http = require('http');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var express = require('express');
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function(req, res, next){
+    console.log('Add access alllow');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
 });
 
-/// error handlers
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+// Let's start managing connections...
+io.on('connection', function(socket) {
+    // Handle 'message' messages
+    socket.on('message', function(message) {
+        log('S --> got message: ', message);
+        // channel-only broadcast...
+        socket.broadcast.to(message.channel).emit('message', message);
     });
-}
+    // Handle 'create or join' messages
+    socket.on('create or join', function(room) {
+        var numClients = io.sockets.clients(room).length;
+        log('S --> Room ' + room + ' has ' + numClients + ' client(s)');
+        log('S --> Request to create or join room', room);
+        // First client joining...
+        if (numClients == 0) {
+            socket.join(room);
+            socket.emit('created', room);
+        } else if (numClients == 1) {
+            // Second client joining...
+            io.sockets.in(room).emit('join', room);
+            socket.join(room);
+            socket.emit('joined', room);
+        } else { // max two clients
+            socket.emit('full', room);
+        }
+    });
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    function log() {
+        var array = [">>> "];
+        for (var i = 0; i < arguments.length; i++) {
+            array.push(arguments[i]);
+        }
+        socket.emit('log', array);
+    }
 });
 
 
-module.exports = app;
+server.listen(3000);
